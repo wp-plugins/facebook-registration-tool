8bdcf88f9f960a0603678625cb16fde14bb27048
mikemarr <mikemarr@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-06 18:52:13 +0000

    patching wrong path
    git-svn-id: https://plugins.svn.wordpress.org/facebook-registration-tool/trunk@329453 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/fbreg.js.php
+++ js/fbreg.js.php
@@ -8,7 +8,7 @@ jQuery(document).ready(function() {
 	jQuery('p.register').detach();
 	jQuery('form#registerform').after(jQuery('<p>').css('padding-top', '10px').html('A password will be e-mailed to you.'));
 	var fb_register = jQuery('<fb:registration></fb:registration>').attr('fields', "[{'name':'name'},{'name':'email'},{'name':'username','description':'Username','type':'text'}]");
-	fb_register.attr('redirect_uri', '<?php echo plugins_url('/facebook-registration/register.php'); ?>');
+	fb_register.attr('redirect_uri', '<?php echo plugins_url('/facebook-registration-tool/register.php'); ?>');
 	fb_register.attr('width', '530');
 	jQuery('form#registerform').after(jQuery(fb_register)).detach();
 	
